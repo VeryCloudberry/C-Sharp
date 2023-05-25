59eeaa81351a25c3e81915ecc76b0635b5c166e4
Alina <smila_@mail.ru>
2023-05-25 07:52:16 +0300

    homework1

---
+++ HW1/Program.cs
@@ -0,0 +1,54 @@
+﻿﻿// Task 1. Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.Write("Input first number ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if(number1 > number2)
+// {
+//     Console.WriteLine($"max = {number1}");
+// }
+// else
+// {
+//     Console.WriteLine($"max = {number2}");
+// }
+
+//Task 2. Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+// Console.Write("Input first number ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input third number ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+// if (num1 > max) max = num1;
+// if (num2 > max) max = num2;
+// if (num3 > max) max = num3;
+// Console.Write($"{num1}, {num2}, {num3} -> max = ");
+// Console.WriteLine(max);
+
+// Task 3. Программа, которая на вход принимает число и выдаёт, является ли оно чётным
+
+// Console.WriteLine ("Input your number ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number % 2 == 0)
+// {
+//     Console.WriteLine($"{number} is even");
+// }
+// else
+// {
+//     Console.WriteLine($"{number} is not even");
+// }
+
+// Task 4. Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine("Input your number ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if(N < 0) Console.WriteLine("Invalid input n");
+// int a = 1;
+// while(a <= N)
+// {
+//     if(a % 2 ==0) Console.Write($"{a}, ");
+//     a++;
+// }
